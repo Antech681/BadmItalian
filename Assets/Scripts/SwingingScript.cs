2f765f0347d361bde2341fd6e60ad95097226518
Jaesda Kim <signupdump1@gmail.com>
2018-08-01 14:44:04 +1200

    Implemented Hit Buttons

---
+++ Assets/Scripts/SwingingScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingingScript : MonoBehaviour {
+    
+    public RacquetControl racCon;
+    private Rigidbody rb;
+
+    public float hitForce;
+
+    public float velocityX;
+    public float velocityY;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        velocityY = rb.velocity.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(racCon.hitLeft))
+        {
+            //rb.AddForce(new Vector3(-hitForce, 0, 0), ForceMode.VelocityChange);
+            rb.velocity = new Vector3(-hitForce, velocityY);
+        }
+        if (Input.GetKeyDown(racCon.hitRight))
+        {
+            //rb.AddForce(new Vector3(hitForce, 0, 0), ForceMode.VelocityChange);
+            rb.velocity = new Vector3(hitForce, velocityY);
+        }
+    }
+}
