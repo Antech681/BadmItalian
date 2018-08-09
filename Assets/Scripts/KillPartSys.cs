2fc0c300fd4b164455528ff0ff285d735ce3efc8
Jaesda Kim <signupdump1@gmail.com>
2018-08-09 14:54:25 +1200

    Particles Automatically Die

---
+++ Assets/Scripts/KillPartSys.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPartSys : MonoBehaviour {
+
+    private ParticleSystem partSys;
+    private float duration;
+
+	// Use this for initialization
+	void Start () {
+        partSys = GetComponent<ParticleSystem>();
+        duration = partSys.startLifetime;
+        Destroy(gameObject, duration);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
