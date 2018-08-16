db6614a4d263019eafbcd40b80bdfc41f7318cfe
Jaesda Kim <signupdump1@gmail.com>
2018-08-16 13:22:14 +1200

    Fullscreen Toggle Prefab Added

---
+++ Assets/Scripts/Fullscreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fullscreen : MonoBehaviour {
+    
+    public void SetFullScreen(bool fullscreen)
+    {
+        Screen.fullScreen = fullscreen;
+        if (Screen.fullScreen)
+        {
+            Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+        }
+        else
+        {
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+        }
+    }
+}
