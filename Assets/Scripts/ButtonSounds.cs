4da5b8a9f898356675613f0c8f853222f1037593
Jaesda Kim <signupdump1@gmail.com>
2018-08-17 13:24:24 +1200

    Added Button Click SFX
    + ButtonSounds.css. When you click a button, play the sound. Woop.

---
+++ Assets/Scripts/ButtonSounds.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSounds : MonoBehaviour {
+
+    private SFXManager sfxMan;
+
+	// Use this for initialization
+	void Start () {
+        sfxMan = FindObjectOfType<SFXManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayClickSound()
+    {
+        sfxMan.buttonHover.pitch = Random.Range(0.8f, 1.2f);
+        sfxMan.buttonHover.Play();
+    }
+}
