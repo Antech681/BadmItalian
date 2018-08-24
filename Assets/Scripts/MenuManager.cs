5014c893fbe5c48d0b6725886de786e2221d5bfc
Unknown <eli.j.t.jackson@gmail.com>
2018-08-24 14:10:22 +1200

    Main Menu Adjustments

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    public GameObject mainPanel;
+    public GameObject creditsPanel;
+
+    public Animator anim;
+
+    public GameObject creditsText;
+    public bool movingCredits;
+
+    void Update()
+    {
+
+    }
+
+    public void Credits()
+    {
+        mainPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+        anim.SetBool("creditsIsRolling", movingCredits);
+        movingCredits = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
