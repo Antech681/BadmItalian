93ac4990dcc18fb1577d26a1edabd5c83eb652eb
Jaesda Kim <signupdump1@gmail.com>
2018-08-24 14:49:36 +1200

    Updated SettingsCanvas Prefab

--- Assets/Scripts/SettingsTabs.cs
+++ Assets/Scripts/SettingsTabs.cs
@@ -8,15 +8,22 @@ public class SettingsTabs : MonoBehaviour {
     public Transform visualsTab;
     public Transform controlsTab;
     public Transform audioTab;
+
+    public MuteScript muteScript;
     
 	void Start () {
         visualsTab = transform.Find("SettingsVisualsTab");
         controlsTab = transform.Find("SettingsControlsTab");
         audioTab = transform.Find("SettingsAudioTab");
         visualsTab.gameObject.SetActive(true);
+        controlsTab.gameObject.SetActive(true);
+        audioTab.gameObject.SetActive(true);
+        muteScript.volumeSlider = GameObject.Find("VolumeControlBar").GetComponent<Slider>();
+        muteScript.volMan = GameObject.Find("VolumeManager").GetComponent<VolumeManager>();
+        muteScript.toggle = GameObject.Find("MuteToggle").GetComponent<Toggle>();
         controlsTab.gameObject.SetActive(false);
         audioTab.gameObject.SetActive(false);
-	}
+    }
 	
 	void Update () {
 		
